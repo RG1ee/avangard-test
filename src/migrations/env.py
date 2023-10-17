630c57a8ff4cf975b9fd1c556f4809a67f92c2d2
RG1ee <niwows@yandex.ru>
2023-10-17 17:51:10 +0300

    create model for user and create migration

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -5,7 +5,7 @@ from flask import current_app
 
 from alembic import context
 
-from src.models import User
+from src.apps.users.models import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
