a484f8b177abe9cc6c1ac1130eae2fe1021dfaf8
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-10-17 17:52:01 +0300

    create model for user and create migration (#3)

---
+++ src/apps/users/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.settings.config_database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255))
+    hashed_password: Mapped[str]
+
+    def __str__(self) -> str:
+        return self.username
